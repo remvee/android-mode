37c3c8e77bc6d1e2790366d0e2bb124a3c9513fb
Remco van 't Veer <rwvtveer@xs4all.nl>
2009-02-20 22:33:52 +0100

    copyright and license

--- android-mode.el
+++ android-mode.el
@@ -1,3 +1,22 @@
+;;; android-mode.el -- Minor mode for Android application development
+
+;; Copyright (C) 2009 R.W van 't Veer
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
 (defgroup android-mode nil
   "A minor mode for Android application development"
   :prefix "android-mode-"
