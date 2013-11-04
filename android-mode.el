f40c972f2becebfdf69e6e1ae04b4a945db985ea
Remco van 't Veer <remco@remworks.net>
2013-11-04 16:48:12 +0100

    Bump version.

--- android-mode.el
+++ android-mode.el
@@ -5,7 +5,7 @@
 ;; Author: R.W. van 't Veer
 ;; Created: 20 Feb 2009
 ;; Keywords: tools processes
-;; Version: 0.2.4
+;; Version: 0.2.5
 ;; URL: https://github.com/remvee/android-mode
 
 ;; Contributors:
