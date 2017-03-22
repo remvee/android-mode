4bcba5e4a406ed433065faef2b5fe4566838d87d
Remco van 't Veer <remco@remworks.net>
2017-03-22 15:40:45 +0100

    Add contributor

--- android-mode.el
+++ android-mode.el
@@ -19,6 +19,7 @@
 ;;   Hiroo Matsumoto
 ;;   K. Adam Christensen
 ;;   Haden Pike
+;;   Camilo QS
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
